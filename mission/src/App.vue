2d2cc10874be1f3486b2d9362474b77a24beef93
Soohyeon02 <tngusr@naver.com>
2022-01-15 01:01:32 +0900

    conflict 해결

--- mission/src/App.vue
+++ mission/src/App.vue
@@ -1,15 +1,5 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
-  <div id="app">
-  <input v-model="message">
-  <button @click="changeInput">제출</button>
-  <button @click="noticeInputAndNumber">알림</button>
-  <p>{{ message }}</p>
-  </div>
+  <router-view />
 </template>
 
 <script>
@@ -42,18 +32,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
