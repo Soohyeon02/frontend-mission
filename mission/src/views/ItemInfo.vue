11bcec49d75aaf674128205940b0b0b1d8652428
Soohyeon02 <tngusr@naver.com>
2022-01-22 02:18:28 +0900

    피드백 내용 반영

--- mission/src/views/ItemInfo.vue
+++ mission/src/views/ItemInfo.vue
@@ -7,31 +7,27 @@
     <img class="sellerImg" src="@/assets/sellerImg.png">
     <img class="bookmarkImg" src="@/assets/bookmarkImg.png">
     <div class="sellerName">
-    코트맛집
+    {{ seller.sellerName }}
     </div>
     <br>
     <div class="hashtag">
-    #남성&nbsp;&nbsp;&nbsp;#코트
+    {{ seller.hashtag1 }}&nbsp;&nbsp;&nbsp;{{ seller.hashtag2 }}
     </div>
   </div>
   <div class="itemContent">
-    <h2 class="title">세미 오버핏 트렌치 코트</h2>
+    <h2 class="title">{{ title }}</h2>
     <div class="priceInfo">
-      <span class="discountRate">{{ discountRate }}%</span>
-      <span class="price">{{ price }}원</span>
-      <span class="beforePrice">{{beforePrice}}원</span>
+      <span class="discountRate">{{ price.discountRate }}%</span>
+      <span class="price">{{ price.price }}원</span>
+      <span class="beforePrice">{{price.beforePrice}}원</span>
     </div>
     <div class="itemInfo">
-      <h3 class="itemInfoTitle">상품정보</h3>
-      <div class="explainText1">남친룩의 정석 코디를 위한 트렌치 코트!</div>
+      <h3 class="itemInfoTitle">{{ itemInfoTitle }}</h3>
+      <div class="briefExplainText">남친룩의 정석 코디를 위한 트렌치 코트!</div>
       <img class="itemInfoImg" src="@/assets/itemInfoImg.png">
       <img class="itemInfoDetailImg" src="@/assets/itemInfoDetailImg.png">
-      <div class="explainText2">
-        - 특유의 여유있는 세미 오버 사이즈 실루엣
-        <br>
-        - 정교한 패턴 작업으로 세심하게 라인을 완성
-        <br>
-        - 간절기 아우터에 적합한 두께감과 뛰어난 착용감
+      <div class="fullExplainText">
+        {{ fullExplainText }}
       </div>
     </div>
   </div>
@@ -41,27 +37,27 @@
     </h3>
     <div class="reviewContent1">
       <img class="reviewImg1" src="@/assets/reviewImg1.png">
-      <span class="reviewerName1">{{ reviewerName1 }}</span>
+      <span class="reviewerName1">{{ review.reviewerName1 }}</span>
       &nbsp;
-      <span class="reviewDat1">{{ reviewDate1 }}</span>
+      <span class="reviewDat1">{{ review.reviewDate1 }}</span>
       <br>
-      <span class="reviewTitle1">{{ reviewTitle1 }}</span>
+      <span class="reviewTitle1">{{ review.reviewTitle1 }}</span>
       <br>
-      <span class="reviewMessage1">{{ reviewMessage1 }}</span>
+      <span class="reviewMessage1">{{ review.reviewMessage1 }}</span>
     </div>
     <div class="reviewContent2">
-      <span class="reviewerName2">{{ reviewerName2 }}</span>
+      <span class="reviewerName2">{{ review.reviewerName2 }}</span>
       &nbsp;
-      <span class="reviewDat2">{{ reviewDate2 }}</span>
+      <span class="reviewDat2">{{ review.reviewDate2 }}</span>
       <br>
-      <span class="reviewTitle2">{{ reviewTitle2 }}</span>
+      <span class="reviewTitle2">{{ review.reviewTitle2 }}</span>
       <br>
-      <span class="reviewMessage2">{{ reviewMessage2 }}</span>
+      <span class="reviewMessage2">{{ review.reviewMessage2 }}</span>
     </div>
   </div>
   <br><br>
   <div class="footer">
-    <button class="buyBotton">{{ price }}원</button>
+    <button class="buyBotton">{{ price.price }}원</button>
   </div>
 </div>
 </template>
@@ -71,17 +67,30 @@ export default {
   name: 'ItemInfoPage',
   data() {
     return {
-      discountRate: 30,
-      price: 140000,
-      beforePrice: 200000,
-      reviewerName1: 'jam******',
-      reviewDate1: '2022.01.06.',
-      reviewTitle1: '착용감이 좋네요!',
-      reviewMessage1: '입었을 때 편해서 앞으로 자주 입을 것 같아요~',
-      reviewerName2: 'yug*******',
-      reviewDate2: '2022.01.03.',
-      reviewTitle2: '느낌 있어요!',
-      reviewMessage2: '입었을 때 연출할 수 있는 분위기가 좋아요!',
+      seller: {
+        sellerName: '코트맛집',
+        hashtag1: '#남성',
+        hashtag2: '#코트',
+      },
+      title: '세미 오버핏 트렌치 코트',
+      itemInfoTitle: '상품정보',
+      briefExplainText: '남친룩의 정석 코디를 위한 트렌치 코트!',
+      fullExplainText: '특유의 여유있는 세미 오버 사이즈 실루엣을 가지고 있습니다. 정교한 패턴 작업으로 세심하게 라인을 완성하고, 간절기 아우터에 적합한 두께감과 뛰어난 착용감을 자랑하는 제품입니다!',
+      price: {
+        discountRate: 30,
+        price: 140000,
+        beforePrice: 200000,
+      },
+      review: {
+        reviewerName1: 'jam******',
+        reviewDate1: '2022.01.06.',
+        reviewTitle1: '착용감이 좋네요!',
+        reviewMessage1: '입었을 때 편해서 앞으로 자주 입을 것 같아요~',
+        reviewerName2: 'yug*******',
+        reviewDate2: '2022.01.03.',
+        reviewTitle2: '느낌 있어요!',
+        reviewMessage2: '입었을 때 연출할 수 있는 분위기가 좋아요!',
+      },
     };
   },
   methods: {
@@ -98,7 +107,7 @@ export default {
   width: 100%;
   height: 100%;
   max-width: 375px;
-  max-height: 375px
+  max-height: 375px;
 }
 .sellerInfo {
   height: 50px;
@@ -134,12 +143,14 @@ export default {
   display: flex;
   margin-top: 5px;
   margin-right: 13px;
+  max-width: 375px;
 }
 .title {
   text-align: left;
   margin-left: 10px;
   width: 100%;
   max-height: 20px;
+  max-width: 375px;
 }
 .discountRate {
   float: left;
@@ -168,6 +179,7 @@ export default {
     max-width: 375px;
 }
 .itemInfoTitle {
+  max-width: 375px;
   margin-top: 20px;
   margin-left: 10px;
   text-align: left;
@@ -175,7 +187,7 @@ export default {
   height: 10px;
   font-weight: lighter;
 }
-.explainText1 {
+.briefExplainText {
   margin-top: 30px;
   margin-left: 10px;
   font-weight: bold;
@@ -188,15 +200,15 @@ export default {
 }
 .itemInfoDetailImg {
   width: 100%;
+  max-width: 375px;
 }
-.explainText2 {
+.fullExplainText {
   text-align: left;
   margin-top: 20px;
   margin-left: 10px;
-  line-height: 40px;
 }
 .review {
-  max-width: 100%;
+  max-width: 375px;
   width: 100%;
 }
 .reviewTitle {
@@ -204,7 +216,7 @@ export default {
   margin-left: 10px;
 }
 .reviewContent1 {
-  max-width: 100%;
+  max-width: 375px;
   line-height: 30px;
   text-align: left;
   margin-left: 10px;
@@ -217,14 +229,14 @@ export default {
   font-size: 10px;
 }
 .reviewImg1 {
-  max-width: 100%;
+  max-width: 375px;
   max-height: 80px;
   display: flex;
   float: right;
   margin-right: 10px;
 }
 .reviewContent2 {
-  max-width: 100%;
+  max-width: 375px;
   line-height: 30px;
   text-align: left;
   margin-left: 10px;
@@ -246,5 +258,6 @@ export default {
   width: 100%;
   height: 60px;
   text-align: center;
+  max-width: 375px;
 }
 </style>
